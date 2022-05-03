770609ed6a247305c9ca8681d3ff62ff78311cec
Chris.c <cjs120dd@gmail.com>
2022-05-03 17:22:30 +0900

    데이터 이름 수정

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const database = [
-    {id: "akjsdk", username: "chris", password: "asd123", birthday: 20211122, signup_date: "2021-11-12"},
+    {id: "akjsdk", username: "chris", password: "asd123", birth: 20211122, signup_date: "2021-11-12"},
 ];
 
 module.exports = database;
